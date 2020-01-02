e7ec91f3b76b47f539f815fed301888e4e53c326
Dimitar Nenkov <55156331+f0naX-C@users.noreply.github.com>
2020-01-02 21:55:33 +0200

    Added DOM Manipulations

--- JavaScript Module/JS Advanced/Object Composition/construction-crew.js
+++
@@ -1,7 +0,0 @@
-function constructionCrew(worker) {
-    if(worker.handsShaking){
-        worker.bloodAlcoholLevel += 0.1 * worker.weight * worker.experience;
-        worker.handsShaking = false;
-    }
-    return worker;
-}
